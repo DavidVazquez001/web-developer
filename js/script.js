ae1eb4b1c780414eebb3c4114d358872bf7f2774
DavidVazquez001 <dvazquez.glife@gmail.com>
2024-03-12 13:26:36 -0600

    Se incorporan las paginas en una carpeta

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 // Redirect to contact.html
 function redirectToContact() {
-  window.location.href = 'contacto.html'
+  // window.location.href = './contacto.html'
+
+  if (document.referrer.endsWith('/index.html')) {
+    window.location.href = './pages/contacto.html'
+  } else {
+    window.location.href = './contacto.html'
+  }
 }
