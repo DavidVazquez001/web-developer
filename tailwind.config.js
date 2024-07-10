7c3a2ea84f6d30f549765ce32d4b0b91a3ac4841
DavidVazquez001 <dvazquez.glife@gmail.com>
2024-07-09 19:42:36 -0600

    Arreglar fondo

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
     theme: {
         extend: {
             backgroundImage: {
-                'custom-background': "url('src/assets/fondo-turquesa.jpg')",
+                'custom-background': "url('assets/fondo-turquesa.jpg')",
             },
         },
     },
