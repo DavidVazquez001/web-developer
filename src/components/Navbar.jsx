3804c7309e46c7216c35c3fc6feb522e7ff9fd6b
DavidVazquez001 <dvazquez.glife@gmail.com>
2024-07-10 09:26:49 -0600

    Se agrega el PDF

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
             </div>
             <div className="flex items-center justify-center gap-4 text-2xl">
                 <a
-                    href="src/assets/cv/CV_Jose_David_Martinez_Vazquez.pdf"
+                    href="../assets/cv/CV_Jose_David_Martinez_Vazquez.pdf"
                     download
                 >
                     <FaRegFilePdf />
